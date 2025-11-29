2daa8c5bdd2abe54140a48fbff6e36f3777f1ce5
Abha Singh <abhasingh.02@gmail.com>
2025-11-29 14:04:05 +0530

    making padding small

--- src/pages/FlashcardPage.vue
+++ src/pages/FlashcardPage.vue
@@ -203,7 +203,7 @@
 
             <q-carousel-slide name="Edit">
               <div class="list-and-view q-gutter-md">
-                <q-card flat bordered class="list q-pa-sm">
+                <q-card flat bordered class="list q-pa-xs">
                   <q-card-section class="row items-center justify-between q-pa-sm bg-grey-2">
                     <section class="col q-ml-lg text-purple items-center">
                       <div class="text-h6">List ({{ filtered.length }})</div>
