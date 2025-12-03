99bae06e735fe7685d1f40761330b43c43f5d927
Abha Singh <abhasingh.02@gmail.com>
2025-12-03 15:51:41 +0530

    changing store file

--- src/stores/cardStore.js
+++ src/stores/cardStore.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 
-export const useCharacterStore = defineStore('character', {
+export const useCardStore = defineStore('card', {
   state: () => ({
     savedFlashcards: [],
     selectedFile: [],
